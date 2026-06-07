491d6de989575ee4fa11bed9d86141683a4d27cf
alina <alina230605@gmail.com>
2026-06-07 14:05:35 +0300

    initial

---
+++ RoadLens/RoadLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadLensApp.swift
+//  RoadLens
+//
+//  Created by alina on 07.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct RoadLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
